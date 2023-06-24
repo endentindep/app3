67836d1c15a05ac24f9c2b5b9a8e814f6b89f4f1
SkyBit <nikita1-2013@mail.ru>
2023-06-24 17:53:39 +0400

    commit

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,16 @@
 package app.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import app.model.Timer;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+	@Bean
+	public Timer getTimer() {
+		return new Timer();
+	}
 }
